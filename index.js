34984851effd4d370603a6553814a49857e5fb83
masaless <ndkextreemo@gmail.com>
2020-12-23 18:13:47 -0300

    Rota para responder perguntas criada

--- index.js
+++ index.js
@@ -66,6 +66,18 @@ app.get("/pergunta/:id", (req, res) =>{
        }
     })
 })
+
+ app.post("/responder", (req, res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect(`/pergunta/ ${perguntaId}`) //res.redirect("/pergunta/4")
+    })
+})
+
 app.listen(3030, () => {
     console.log("App Rodando");
 })
